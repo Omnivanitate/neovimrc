ba7262a04cdcfb6dbf7b92f8babca4563bf2fb9e
Omnivanitate <serebraykov.vs@gmail.com>
2026-03-02 22:34:52 +0000

    add fugitive undotree zenmode

---
+++ lua/core/plugins/zenmode.lua
@@ -0,0 +1,29 @@
+return {
+    'folke/zen-mode.nvim',
+    keys = {
+        {'<leader>zz', function()
+            require('zen-mode').setup({
+                window = {
+                    width = 90,
+                    options = { }
+                },
+            })
+            require('zen-mode').toggle()
+            vim.wo.wrap = false
+            vim.wo.number = true
+            vim.wo.rnu = true
+        end, {}},
+        {'<leader>zZ', function() 
+            require('zen-mode').setup({
+                window = {
+                    window = 80,
+                    options = { }
+                },
+            })
+            require('zen-mode').toggle()
+            vim.wo.wrap = false
+            vim.wo.number = false
+            vim.wo.rnu = false
+        end, {}}
+    }
+}
