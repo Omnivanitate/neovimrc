d9e9a9fe662b7a8e9abd4ecefa2e5f526e752d5b
Omnivanitate <serebraykov.vs@gmail.com>
2026-04-14 07:54:22 +0000

    rename lsp config to the name of lsp

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,18 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      runtime = {
+        version = "LuaJIT",
+      },
+      telemetry = { enabled = false },
+      format = { enable = false },
+    },
+  },
+}
